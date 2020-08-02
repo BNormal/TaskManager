703f36c57379976a8f08bad86c7075f44a729800
Benson <nor15002@byui.edu>
2020-08-02 18:51:59 -0500

    Removed loading image file. Images load from URLs and only load necessary images

--- dreambot/src/TaskManager/scripts/misc/GETradeGUI.java
+++ dreambot/src/TaskManager/scripts/misc/GETradeGUI.java
@@ -4,7 +4,6 @@ import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
@@ -26,13 +25,12 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.imageio.ImageIO;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
@@ -50,10 +48,15 @@ public class GETradeGUI {
 	private JFrame frame;
 	private Map<Integer, DisplayItem> itemList = new HashMap<Integer, DisplayItem>();
 	private JTextField txtItemName;
+	private JList<DisplayItem> listItems;
 	private DefaultListModel<DisplayItem> modelItems;
 	private int itemId = -1;
 	private JPanel itemImage;
 	private JCheckBox chckbxF2P;
+	private ImageIcon f2pIcon;
+	private ImageIcon membersIcon;
+	private Thread spriteThread;
+	private long typeDelay = 0;
 
 	/**
 	 * Launch the application.
@@ -80,10 +83,30 @@ public class GETradeGUI {
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
-		parseJson();
 		initialize(title);
 	}
 
+	public void loadSprites() {
+		String text = txtItemName.getText();
+		int loads = 0;
+		for (int i = 0; i < modelItems.size(); i++) {
+			DisplayItem item = modelItems.get(i);
+			if (item.getSprite() == null) {
+				loads++;
+				try {
+					ImageIcon icon = new ImageIcon(new URL("https://www.osrsbox.com/osrsbox-db/items-icons/" + item.getID() + ".png"));
+					item.setSprite(icon);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (!txtItemName.getText().equals(text))
+				break;
+		}
+		if (loads > 0)
+			listItems.updateUI();
+	}
+	
 	public void parseJson() {
 		try {
 			Gson gson = new Gson();
@@ -116,6 +139,7 @@ public class GETradeGUI {
 		frame.getContentPane().setLayout(null);
 		
 		txtItemName = new JTextField();
+		txtItemName.setEnabled(false);
 		txtItemName.setColumns(10);
 		txtItemName.setBounds(274, 16, 160, 20);
 		txtItemName.addKeyListener(new KeyAdapter() {
@@ -123,6 +147,7 @@ public class GETradeGUI {
 			public void keyReleased(KeyEvent arg0) {
 				String itemName = txtItemName.getText();
 				if (itemName != null && !itemName.equals("") && itemName.length() > 2) {
+					typeDelay = System.currentTimeMillis();
 					updateList(itemName);
 				} else {
 					modelItems.clear();
@@ -140,7 +165,7 @@ public class GETradeGUI {
 		
 		modelItems = new DefaultListModel<>();
 		
-		JList<DisplayItem> listItems = new JList<DisplayItem>(modelItems);
+		listItems = new JList<DisplayItem>(modelItems);
 		listItems.setCellRenderer(new ItemImageRenderer());
 		listItems.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent arg0) {
@@ -158,26 +183,25 @@ public class GETradeGUI {
 		scrollPane.setBounds(206, 70, 268, 161);
 		frame.getContentPane().add(scrollPane);
 		
-		JButton btnSelect = new JButton("Select Item");
-		btnSelect.setBounds(206, 240, 268, 23);
-		frame.getContentPane().add(btnSelect);
-		
 		itemImage = new JPanel() {
 			private static final long serialVersionUID = -4793964038601761016L;
 
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				BufferedImage sprite = null;
-				try {
-					if (itemId > -1) {
-						sprite = ImageIO.read(new URL("https://www.osrsbox.com/osrsbox-db/items-icons/" + itemId + ".png"));
+				ImageIcon sprite = null;
+				if (itemId > -1) {
+					sprite = itemList.get(itemId).getSprite();
+					if (sprite == null) {
+						try {
+							sprite = new ImageIcon(new URL("https://www.osrsbox.com/osrsbox-db/items-icons/" + itemId + ".png"));
+						} catch (MalformedURLException e) {
+							e.printStackTrace();
+						}
 					}
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
 				if (sprite != null) {
-					g.drawImage(sprite, (itemImage.getWidth() - sprite.getWidth()) / 2 + 1, (itemImage.getHeight() - sprite.getHeight()) / 2, null);
+					g.drawImage(sprite.getImage(), (itemImage.getWidth() - sprite.getIconWidth()) / 2 + 1, (itemImage.getHeight() - sprite.getIconHeight()) / 2, null);
 				}
 			}
 		};
@@ -186,6 +210,7 @@ public class GETradeGUI {
 		frame.getContentPane().add(itemImage);
 		
 		chckbxF2P = new JCheckBox("Display F2P items only");
+		chckbxF2P.setEnabled(false);
 		chckbxF2P.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String itemName = txtItemName.getText();
@@ -199,13 +224,43 @@ public class GETradeGUI {
 		chckbxF2P.setFocusable(false);
 		chckbxF2P.setBounds(206, 40, 138, 23);
 		frame.getContentPane().add(chckbxF2P);
+		
+		JLabel lblLoading = new JLabel("Loading data...");
+		lblLoading.setBounds(10, 11, 127, 14);
+		frame.getContentPane().add(lblLoading);
+		Thread thread = new Thread() {
+			public void run() {
+				try {
+					f2pIcon = new ImageIcon(new URL("https://static.wikia.nocookie.net/2007scape/images/6/6f/Free-to-play_icon.png"));
+					membersIcon = new ImageIcon(new URL("https://static.wikia.nocookie.net/2007scape/images/9/9c/Member_icon.png"));
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+				parseJson();
+				chckbxF2P.setEnabled(true);
+				txtItemName.setEnabled(true);
+				lblLoading.setVisible(false);
+			}
+		};
+		thread.start();
 	}
-
+	
 	public void updateList(String itemName) {
 		modelItems.clear();
 		for (DisplayItem item : itemList.values()) {
 			if (item.getName().toLowerCase().contains(itemName.toLowerCase()) && (chckbxF2P.isSelected() && !item.isMembers() || !chckbxF2P.isSelected()))
 				modelItems.addElement(item);
+		}
+		if (modelItems.size() > 0) {
+			if (spriteThread == null || !spriteThread.isAlive()) {
+				spriteThread = new Thread("Updating") {
+					public void run() {
+						while (System.currentTimeMillis() - typeDelay < 500);
+						loadSprites();
+					}
+				};
+				spriteThread.start();
+			}
 		}
 	}
 	
@@ -219,7 +274,6 @@ public class GETradeGUI {
 	        char[] chars = new char[1024];
 	        while ((read = reader.read(chars)) != -1)
 	            buffer.append(chars, 0, read); 
-
 	        return buffer.toString();
 	    } finally {
 	        if (reader != null)
@@ -235,12 +289,14 @@ public class GETradeGUI {
 		@Override
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         	JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-			ImageIcon icon = null;
-			try {
-				icon = new ImageIcon(new URL("https://www.osrsbox.com/osrsbox-db/items-icons/" + ((DisplayItem) value).getID() + ".png"));
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
+			DisplayItem item = ((DisplayItem) value);
+        	ImageIcon icon = item.getSprite();
+			if (icon != null && membersIcon != null && f2pIcon != null)
+				icon = mergeIcons(icon, item.isMembers() ? membersIcon : f2pIcon);
+			if (icon == null) {//Width:36 Height:32
+				icon = new ImageIcon(new BufferedImage(36, 32, BufferedImage.TYPE_INT_ARGB));
 			}
+			
             label.setIcon(icon);
             label.setHorizontalTextPosition(JLabel.RIGHT);
             label.setFont(font);
@@ -248,15 +304,27 @@ public class GETradeGUI {
         }
     }
 	
+	public ImageIcon mergeIcons(ImageIcon icon1, ImageIcon icon2) {
+        // For simplicity we will presume the images are of identical size
+        final BufferedImage combinedImage = new BufferedImage(icon1.getIconWidth(), icon1.getIconHeight(), BufferedImage.TYPE_INT_ARGB );
+        Graphics2D g = combinedImage.createGraphics();
+        g.drawImage(icon1.getImage(), 0, 0, null);
+        g.drawImage(icon2.getImage(), icon1.getIconWidth() - icon2.getIconWidth(), icon1.getIconHeight() - icon2.getIconHeight(), null);
+        g.dispose();
+        return new ImageIcon(combinedImage);
+	}
+	
 	public class DisplayItem {
 		private int id;
 		private String name;
 		private boolean members;
+		private ImageIcon sprite;
 	    
 		public DisplayItem(int id, String name, boolean members) {
 			this.id = id;
 			this.name = name;
 			this.members = members;
+			sprite = null;
 		}
 
 	    public int getID() {
@@ -281,6 +349,14 @@ public class GETradeGUI {
 
 		public void setMembers(boolean members) {
 			this.members = members;
+		}
+		
+		public ImageIcon getSprite() {
+			return sprite;
+		}
+		
+		public void setSprite(ImageIcon sprite) {
+			this.sprite = sprite;
 		}
 		
 		@Override
